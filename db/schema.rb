b57acbfea59239a4aba001f21886227bcbf87af6
R.nakamura <krw.psh2196@gmail.com>
2020-07-27 10:52:11 +0000

    overwork_timeカラムをattendanceモデルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200727103803) do
+ActiveRecord::Schema.define(version: 20200727105114) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20200727103803) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "today_work"
+    t.datetime "overwork_time"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
