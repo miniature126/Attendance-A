dac692e4ad0a82628bc3896626f1eaf0ee2fc394
R.nakamura <krw.psh2196@gmail.com>
2020-07-27 10:47:26 +0000

    superiorカラムを追加、管理者ユーザー、上長ユーザー、一般ユーザーを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,25 @@
 # coding: utf-8
 
-User.create!(name: "admin",
+User.create!(name: "Admin",
              email: "sample@email.com",
              password: "password",
              password_confirmation: "password",
              admin: true)
 
+User.create!(name: "Superior1",
+             email: "sample-a@email.com",
+             password: "password",
+             password_confirmation: "password",
+             superior: true)
+
+User.create!(name: "Superior2",
+             email: "sample-b@email.com",
+             password: "password",
+             password_confirmation: "password",
+             superior: true)
+
 5.times do |n|
-  name = Faker::Name.name
+  name = "User#{n+1}"
   email = "sample-#{n+1}@email.com"
   password = "password"
   User.create!(name: name,
