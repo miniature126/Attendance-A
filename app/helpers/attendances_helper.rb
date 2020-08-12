fc53fd7ccefebad5fcd771b56dc64c69b2239772
R.nakamura <krw.psh2196@gmail.com>
2020-08-12 16:56:53 +0900

    時間外時間計算メソッドを修正

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -15,13 +15,13 @@ module AttendancesHelper
     format("%.2f", (((finish.floor_to(15.minute) - start.floor_to(15.minute)) / 60) / 60.0))
   end
   
-  #指定勤務終了時間と退勤時間を受け取り、時間外時間を計算して返す
-  def overwork_times(desig_finish, finish, next_day, worked)
+  #指定勤務終了時間と終了予定時間を受け取り、時間外時間を計算して返す
+  def overwork_times(desig_finish, overwork, next_day, worked)
     regular_time = worked.midnight.since(desig_finish.seconds_since_midnight) #worked_onの日付とdesig_finish_worktimeの時間を組み合わせる
     if next_day #「翌日」にチェックが入っている時
-      format("%.2f",(((finish.floor_to(15.minute) - regular_time.floor_to(15.minute)) / 60) / 60.0))
+      format("%.2f",((((overwork.floor_to(15.minute) + 1.day) - regular_time.floor_to(15.minute)) / 60) / 60.0))
     else
-      format("%.2f",(((finish.floor_to(15.minute) - regular_time.floor_to(15.minute)) / 60) / 60.0))
+      format("%.2f",(((overwork.floor_to(15.minute) - regular_time.floor_to(15.minute)) / 60) / 60.0))
     end
   end
 end
