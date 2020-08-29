15a48946d3a9b341403a2bf9cbc5714b316009fc
R.nakamura <krw.psh2196@gmail.com>
2020-08-29 15:15:24 +0900

    instructor_confirmationカラムのデフォルト値を削除

--- db/migrate/20200818105656_change_default_instructor_confirmation_to_attendances.rb
+++ db/migrate/20200818105656_change_default_instructor_confirmation_to_attendances.rb
@@ -1,9 +1,9 @@
 class ChangeDefaultInstructorConfirmationToAttendances < ActiveRecord::Migration[5.1]
   def up
-    change_column :attendances, :instructor_confirmation, :integer, default: 2
+    change_column :attendances, :instructor_confirmation, :integer, default: nil
   end
   
   def down
-    change_column :attendances, :instructor_confirmation, :integer
+    change_column :attendances, :instructor_confirmation, :integer, default: 2
   end
 end
