a55862c50971e942aa8d0da3fad3ef6f871a4a17
R.nakamura <krw.psh2196@gmail.com>
2020-09-07 20:04:59 +0900

    update_change_noticeアクションを編集

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,6 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
-  before_action :set_superior_attendances, only: :edit_overwork_request
+  # before_action :set_superior_attendances, only: :edit_overwork_request
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :superior_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
@@ -51,15 +51,28 @@ class AttendancesController < ApplicationController
   end
   
   def edit_change_notice #勤怠変更の承認
-    @superior = User.find(params[:id])
+    @superior = User.find(params[:id]) #送信先に@superiorのidが含まれるため必要
     @users = User.all
   end
   
   def update_change_notice
+    @superior = User.find(params[:id])
+    attendances_params.each do |id, item|
+      @attendance = Attendance.find(id) #送信先に@superiorのidが含まれるため必要
+      if ActiveRecord::Type::Boolean.new.cast(params[:user][:attendances][id][:change_attendances_reflection]) #string型→boolean型に
+        if @attendance.update_attributes(item)
+          flash[:success] = "勤怠変更申請の情報を更新しました。"
+        else
+          flash[:danger] = "更新をキャンセルしました。"
+        end
+      end
+    end
   end
   
   # URLのidにはattendanceのidが入っている
   def edit_overwork_request
+    @attendance = Attendance.find(params[:id]) #paramsから取得したユーザー
+    @superior = User.where(superior: true).where.not(id: @attendance.user_id)
   end
 
   def update_overwork_request
@@ -81,7 +94,7 @@ class AttendancesController < ApplicationController
     @superior = User.find(params[:id])
     overwork_params.each do |id, item| #update_one_monthアクション参考
       @attendance = Attendance.find(id)
-      if ActiveRecord::Type::Boolean.new.cast(params[:user][:attendances][id][:reflection]) #refはstring型なのでboolean型に変換
+      if ActiveRecord::Type::Boolean.new.cast(params[:user][:attendances][id][:overwork_reflection]) #string型→boolean型に
         if @attendance.update_attributes(item)
           flash[:success] = "情報を更新しました。"
         else
@@ -100,19 +113,19 @@ class AttendancesController < ApplicationController
       @user = User.find(@attendance.user_id)
     end
     
-    #上長ユーザーを取得(ログインしているユーザーが上長だった場合は、そのユーザーは除く)
-    def set_superior_attendances
-      @attendance = Attendance.find(params[:id]) #paramsから取得したユーザー
-      @superior = User.where(superior: true).where.not(id: @attendance.user_id)
-    end
+    # #上長ユーザーを取得(ログインしているユーザーが上長だった場合は、そのユーザーは除く)
+    # def set_superior_attendances
+    #   @attendance = Attendance.find(params[:id]) #paramsから取得したユーザー
+    #   @superior = User.where(superior: true).where.not(id: @attendance.user_id)
+    # end
     
     #１ヶ月分の勤怠申請情報を扱う
     def attendances_params
-      params.require(:user).permit(attendances: [:started_at, :finished_at, :note, :applied_attendances_change, :change_attendances_confirmation])[:attendances]
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note, :applied_attendances_change, :change_attendances_confirmation, :change_attendances_reflection])[:attendances]
     end
     
     #残業申請情報を扱う
     def overwork_params
-      params.require(:user).permit(attendances: [:finish_overwork, :next_day, :work_contents, :applied_overwork, :overwork_confirmation, :reflection])[:attendances]
+      params.require(:user).permit(attendances: [:finish_overwork, :next_day, :work_contents, :applied_overwork, :overwork_confirmation, :overwork_reflection])[:attendances]
     end
 end
