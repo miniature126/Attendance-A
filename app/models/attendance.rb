d00a1888b6f0fa63f098a10e9f67c14a7f9c0532
R.nakamura <krw.psh2196@gmail.com>
2020-09-09 20:03:05 +0900

    残業申請機能のバリデーションを追加中

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,6 +3,10 @@ class Attendance < ApplicationRecord
   
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
+  #残業申請、承認
+  validates :work_contents, presence: true, allow_nil: true
+  #勤怠変更申請、承認
+  
   
   #出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
@@ -12,20 +16,14 @@ class Attendance < ApplicationRecord
   validate :started_at_is_invalid_exist_a_finished_at
   #退勤時間が存在する時、終了予定時間の更新は無効
   # validate :finish_overwork_is_invalid_exist_a_finished_at
-  
-  #残業申請、承認
-  
-  
-  #勤怠変更申請、承認
-  
+  #指定勤務終了時間より早い残業終了予定時間は無効
+  validate :finish_overwork_earlier_than_desig_finish_worktime_is_invalid
+  #残業終了予定時間が存在する時、業務処理内容と残業申請送信先も同じく存在する
+  validate :finish_overwork_exist_work_contents_applied_overwork_exist
   
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
-  
-  # def finish_overwork_is_invalid_exist_a_finished_at
-  #   errors.add("退勤しているので、残業申請は無効です") if finished_at.present? && finish_overwork.present?
-  # end
   
   def started_at_then_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
@@ -37,6 +35,24 @@ class Attendance < ApplicationRecord
   def started_at_is_invalid_exist_a_finished_at
     unless worked_on == Date.current
       errors.add(:finished_at, " が必要です") if started_at.present? && finished_at.blank?
+    end
+  end
+  
+  # def finish_overwork_is_invalid_exist_a_finished_at
+  #   errors.add("退勤しているので、残業申請は無効です") if finished_at.present? && finish_overwork.present?
+  # end
+  
+  #指定勤務終了時間より早い残業終了予定時間は無効
+  def finish_overwork_earlier_than_desig_finish_worktime_is_invalid
+    if user.desig_finish_worktime > finish_overwork
+      errors.add(:desig_finish_worktime, "より早い時間は無効です")
+    end
+  end
+  
+  #残業終了予定時間が存在する時、業務処理内容と残業申請送信先も同じく存在する
+  def finish_overwork_exist_work_contents_applied_overwork_exist
+    if finish_overwork.present?
+      errors.add(:work_contents, :applied_overwork, "が必要です") unless work_contents.present? && applied_overwork.present?
     end
   end
 end
