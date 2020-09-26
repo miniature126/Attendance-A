f2113639682a93ae430ea4ec05467ba16a8750f7
R.nakamura <krw.psh2196@gmail.com>
2020-09-26 14:53:52 +0900

    開発環境移行（aws Cloud9→ローカル環境）

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -2,7 +2,7 @@ class ApprovalsController < ApplicationController
   # before_action :set_user, only: [:edit_approval_superior_notice, :update_approval_superior_notice]
   
   def edit_approval_superior_notice
-    @user = User.find(params[:user_id])
+    @users = User.find(params[:user_id])
   end
   
   def update_approval_superior_notice
