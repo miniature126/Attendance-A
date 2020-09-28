c0a4f0a232408b75f6e686d71696b626c79f6f19
Rumi Nakamura <krw.psh2196@gmail.com>
2020-09-28 15:03:32 +0900

    1ヶ月分の勤怠申請機能の実装を再開

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -1,6 +1,10 @@
 class ApprovalsController < ApplicationController
   # before_action :set_user, only: [:edit_approval_superior_notice, :update_approval_superior_notice]
   
+  def update_approval_superior_request
+    
+  end
+  
   def edit_approval_superior_notice
     @users = User.find(params[:user_id])
   end
