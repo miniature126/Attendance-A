6fe77886ab5543e8bb86ec827b34905cedfde9be
Rumi Nakamura <krw.psh2196@gmail.com>
2020-10-09 18:18:56 +0900

    上長側からの1ヶ月分の勤怠情報更新機能が完成

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -8,10 +8,10 @@ class ApprovalsController < ApplicationController
       @approval.save
       @approval.update_attributes!(approval_request_params)
     end
-    flash[:success] = "#{@approval.applied_month.month}月の勤怠承認を申請しました。"
+    flash[:success] = "#{@approval.applied_month.month}月分の勤怠を申請しました。"
     redirect_to user_url(@user)
   rescue ActiveRecord::RecordInvalid #トランザクションエラー分岐
-    flash[:danger] = "#{@approval.applied_month.month}月の勤怠申請をキャンセルしました。"
+    flash[:danger] = "#{@approval.applied_month.month}月分の勤怠申請をキャンセルしました。"
     redirect_to user_url(@user)
   end
   
@@ -22,13 +22,13 @@ class ApprovalsController < ApplicationController
   
   def update_approval_superior_notice
     @superior = User.find(params[:user_id])
-    ActiveRecord::base.transaction do #トランザクション処理開始
+    ActiveRecord::Base.transaction do #トランザクション処理開始
       approval_notice_params.each do |id, item|
         approval = Approval.find(id)
-        approval.update_attributes!(item) if ActiveRecord::Type::Boolean.mew.cast(params[:user][:approvals][id][:approval_superior_reflection]) #string型→boolean型へ変更
+        approval.update_attributes!(item) if ActiveRecord::Type::Boolean.new.cast(params[:user][:approvals][id][:approval_superior_reflection]) #string型→boolean型へ変更
       end
     end
-    flash[:success] = "勤怠を承認しました。"
+    flash[:success] = "1ヶ月分の勤怠申請情報を更新しました。"
     redirect_to user_url(@superior)
   rescue ActiveRecord::RecordInvalid #トランザクション例外処理
     flash[:danger] = "無効なデータがあった為、更新をキャンセルしました。"
@@ -42,6 +42,7 @@ class ApprovalsController < ApplicationController
     end
 
     #一般→上長(上長→上長)の1ヶ月分の勤怠申請情報を扱う
+    # f.fields_for "approvals[]", day do |approval|←この部分が.permit(〇〇: 〜)と一緒
     def approval_notice_params
       params.require(:user).permit(approvals: [:approval_superior_confirmation, :approval_superior_reflection])[:approvals]
     end
