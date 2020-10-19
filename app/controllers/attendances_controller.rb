538befd511eb24ca31854933879af5e881159018
Rumi Nakamura <krw.psh2196@gmail.com>
2020-10-19 20:14:57 +0900

    勤怠ログ絞り込み表示機能の実装を開始

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -13,17 +13,12 @@ class AttendancesController < ApplicationController
     #出勤時間が未登録であることを判定
     if @attendance.started_at.nil?
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
-        #勤怠ログ表示用のレコードを作成(日付と出勤時間のみ)
-        # @attendance.create_correction(date: @attendance.worked_on, attendance_time: @attendance.started_at)
         flash[:info] = "おはようございます！"
       else
         flash[:danger] = UPDATE_ERROR_MSG
       end
     elsif @attendance.finished_at.nil?
       if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
-        #勤怠ログ表示用のレコードを更新(退勤時間)
-        # @attendance.correction.update_attributes(leaving_time: @attendance.finished_at)
-        debugger
         flash[:info] = "お疲れ様でした。"
       else
         flash[:danger] = UPDATE_ERROR_MSG
@@ -42,7 +37,7 @@ class AttendancesController < ApplicationController
         attendance = Attendance.find(id) #レコードを探し格納
         if item[:applied_attendances_change].present? #勤怠変更申請先(上長)のidが存在する場合のみ
           @user.desig_finish_worktime = attendance.worked_on.midnight.since(@user.desig_finish_worktime.seconds_since_midnight)
-          @user.save #@userの指定勤務終了時間の日付を申請している日付に合わせる。合わせないと指定勤務終了時間より早い残業終了予定時間は無効のバリデーションに引っかかる
+          @user.save #@userの指定勤務終了時間の日付を申請している日付に合わせる。(合わせないと指定勤務終了時間より早い残業終了予定時間は無効のバリデーションに引っかかる)
           if attendance.started_at.present? && attendance.finished_at.present? #既に出勤時間と退勤時間が存在する時
             attendance.started_at_before_change = attendance.started_at #出勤時間を変更前出勤時間カラムに移動
             attendance.finished_at_before_change = attendance.finished_at #退勤時間を変更前退勤時間カラムに移動
@@ -71,9 +66,11 @@ class AttendancesController < ApplicationController
     ActiveRecord::Base.transaction do #トランザクションを開始
       attendances_params.each do |id, item|
         attendance = Attendance.find(id)
-        #「変更」にチェックが入っている時は更新
+        #「変更」にチェックが入っている時は更新、更新後は変更カラムを空に
         attendance.update_attributes!(item) if ActiveRecord::Type::Boolean.new.cast(params[:user][:attendances][id][:change_attendances_reflection]) #string型→boolean型に
-        
+        attendance.change_attendances_reflection = nil
+        attendance.save
+
         if attendance.change_attendances_confirmation == 3 #ステータスが承認済みの場合のみ
           if attendance.correction.present?
             @correction = attendance.correction
@@ -143,6 +140,8 @@ class AttendancesController < ApplicationController
         user.desig_finish_worktime = attendance.worked_on.midnight.since(user.desig_finish_worktime.seconds_since_midnight)
         user.save #基本時間の日付を残業申請日に合わせて変更、保存
         attendance.update_attributes!(item) if ActiveRecord::Type::Boolean.new.cast(params[:user][:attendances][id][:overwork_reflection]) #string型→boolean型に(:overwork_reflection→「変更」)
+        attendance.overwork_reflection = nil
+        attendance.save
       end
     end
     flash[:success] = "情報を更新しました。"
