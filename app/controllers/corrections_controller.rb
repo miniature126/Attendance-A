5ac5ef2227ee821ce193214beb3d6b3fe28aabc5
Rumi Nakamura <krw.psh2196@gmail.com>
2020-10-27 21:57:38 +0900

    勤怠ログ表示の条件を修正、キャンセルボタンを追加　

--- app/controllers/corrections_controller.rb
+++ app/controllers/corrections_controller.rb
@@ -3,12 +3,12 @@ class CorrectionsController < ApplicationController
   before_action :set_attendances, only: :attendance_log
 
   def attendance_log
-    if params[:search]["date(1i)"].present? && params[:search]["date(2i)"].present?
-      first_day = Date.new(params[:search]["date(1i)"].to_i, params[:search]["date(2i)"].to_i, params[:search]["date(3i)"].to_i)
-      last_day = first_day.end_of_month
-      @attendances = @attendances.where(worked_on: first_day..last_day)
-    else
-      @attendances
+    if params[:search].present?
+      if params[:search]["date(1i)"].present? && params[:search]["date(2i)"].present?
+        first_day = Date.new(params[:search]["date(1i)"].to_i, params[:search]["date(2i)"].to_i, params[:search]["date(3i)"].to_i)
+        last_day = first_day.end_of_month
+        @attendances = @attendances.where(worked_on: first_day..last_day)
+      end
     end
   end
 
