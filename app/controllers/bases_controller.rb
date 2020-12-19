ede8e0dd05ad597214e56e9d97735091692463c5
Rumi Nakamura <krw.psh2196@gmail.com>
2020-12-19 15:47:54 +0900

    destroyアクションが完成

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,35 +1,43 @@
 class BasesController < ApplicationController
+  before_action :set_base, only: [:update, :destroy]
+
   def index
     @bases = Base.all
   end
   
   def create
     @base = Base.new(base_params)
-    debugger
     if @base.save
       flash[:success] = "拠点情報を作成しました。"
       redirect_to bases_url
     else
-      redirect_to bases_url, flash: { danger: "拠点情報追加をキャンセルしました。入力エラーが#{@base.errors.full_messages.count}件あります。<br>#{@base.errors.full_messages.join("。<br>")}" }
+      flash[danger] = "拠点情報追加をキャンセルしました。入力エラーが#{@base.errors.full_messages.count}件あります。<br>#{@base.errors.full_messages.join("。<br>")}"
+      redirect_to bases_url 
     end
   end
 
   def update
-    @base = Base.find(params[:id])
     if @base.update_attributes(base_params)
       flash[:success] = "拠点情報を修正しました。"
       redirect_to bases_url
     else
-      redirect_to bases_url, flash: { danger: "更新をキャンセルしました。入力エラーが#{@base.errors.full_messages.count}件あります。<br>#{@base.errors.full_messages.join("。<br>")}" }
+      flash[:danger] = "修正をキャンセルしました。入力エラーが#{@base.errors.full_messages.count}件あります。<br>#{@base.errors.full_messages.join("。<br>")}"
+      redirect_to bases_url
     end
   end
   
   def destroy
-    
+    flash[:success] = "拠点番号#{@base.number}のデータを削除しました。"
+    @base.destroy
+    redirect_to bases_url
   end
 
   private
     def base_params
       params.require(:base).permit(:number, :name, :attendance_type)
+    end
+
+    def set_base
+      @base = Base.find(params[:id])
     end
 end
